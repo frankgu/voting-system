381128d926a92e25854850cbc43a0ed5ad19dccc
dongfeng gu <gudongfeng@outlook.com>
2015-02-27 15:04:23 -0500

    test

--- VotingSystem/src/com/run/client.java
+++ VotingSystem/src/com/run/client.java
@@ -11,13 +11,13 @@ public class client {
 	public static void main(String[] arg) {
 
 		//Client client = new Client();
-		
+		//asdf;lkjoiwer
 		// hibernate test
 		User user = new Voter("gdf1992803", "dongfeng","gu","ottawa","k1g4a7");
 		Voter voter = (Voter)user;
 		voter.setCandidateName("hello");
 		
-		Session session = HibernateUtilServer2.getSessionFactory().openSession();
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 		session.save(voter);
 		session.getTransaction().commit();
