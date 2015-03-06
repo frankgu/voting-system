7f6bb3f31562ffbc23d74a07bd14b49ebbbfa6c8
shawn24 <shawn_yao@hotmail.com>
2015-03-06 17:08:22 -0500

    idea

--- VotingSystem/src/com/run/StartServer1.java
+++ VotingSystem/src/com/run/StartServer1.java
@@ -7,8 +7,9 @@ public class StartServer1 {
 	public static void main(String[] arg){
 		
 		Server1 server1 = new Server1("ottawa", 8080, "localhost");
+		//addline name:ip:port to git remote server1List.txt
 		Thread thread = new Thread(server1);
 		thread.start();
-		
+		//on termination, search name:ip:port on remote list, delete line
 	}
 }
