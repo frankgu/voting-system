f727d799750f80ea4ec1b9b781348c9690002ed3
shawn24 <shawn_yao@hotmail.com>
2015-03-14 16:49:34 -0400

    updated serv1 exec

--- VotingSystem/src/com/run/StartServer1.java
+++ VotingSystem/src/com/run/StartServer1.java
@@ -6,7 +6,7 @@ public class StartServer1 {
 
 	public static void main(String[] arg){
 		
-		Server1 server1 = new Server1("test serv", 8088, "go.joyclick.org");
+		Server1 server1 = new Server1("Ottawa Ease", 8080, "127.0.0.1");
 		//addline name:ip:port to git remote server1List.txt
 		Thread thread = new Thread(server1);
 		thread.start();
