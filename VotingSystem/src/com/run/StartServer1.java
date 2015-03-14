bd0e2fe73ce8041650ce3c85f18e29d55418dbaa
shawn24 <shawn_yao@hotmail.com>
2015-03-14 12:49:29 -0400

    upd

--- VotingSystem/src/com/run/StartServer1.java
+++ VotingSystem/src/com/run/StartServer1.java
@@ -6,7 +6,7 @@ public class StartServer1 {
 
 	public static void main(String[] arg){
 		
-		Server1 server1 = new Server1("test serv", 8088, "45.55.169.241");
+		Server1 server1 = new Server1("Ottawa East", 8080, "127.0.0.1");
 		//addline name:ip:port to git remote server1List.txt
 		Thread thread = new Thread(server1);
 		thread.start();
