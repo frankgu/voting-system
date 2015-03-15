36596055027a12f90337c556f100f7fc4ee1a7db
shawn24 <shawn_yao@hotmail.com>
2015-03-15 17:49:01 -0400

    upd

--- VotingSystem/src/com/run/CandidateReg.java
+++ VotingSystem/src/com/run/CandidateReg.java
@@ -20,7 +20,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.SelectionEvent;
 
 import java.util.ArrayList;
