765a03428b00e45335bde282fe5023cd29625172
xianchizou <xianchi.zou@gmail.com>
2015-03-15 16:03:52 -0400

    @testframework

---
+++ VotingSystem/src/com/run/testFrame.java
@@ -0,0 +1,10 @@
+package com.run;
+
+import com.testframework.testframework;
+
+public class testFrame {
+	public static void main(String args[]){
+	testframework tf = new testframework("", "");
+	tf.run();
+	}
+}
