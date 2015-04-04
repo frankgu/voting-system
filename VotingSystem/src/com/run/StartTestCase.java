242f59305405da07b79f253bb2aaae0479735854
shawn24 <shawn_yao@hotmail.com>
2015-04-04 16:30:45 -0400

    upd

--- VotingSystem/src/com/run/StartTestCase.java
+++ VotingSystem/src/com/run/StartTestCase.java
@@ -1,10 +1,44 @@
 package com.run;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import com.functions.Transmission;
 import com.testframework.testframework_launch;
 
 public class StartTestCase {
 	public static void main(String args[]){
-		testframework_launch run = new testframework_launch();
-		testframework_launch.main(args);
+		//testframework_launch run = new testframework_launch();
+		//testframework_launch.main(args);
+		
+		DatagramSocket bSocket;
+		try {
+			bSocket = new DatagramSocket();
+			byte[] buffer = new byte[80];
+			InetAddress bhost = InetAddress.getByName("go.joyclick.org");
+			
+	        DatagramPacket request = new DatagramPacket(buffer,
+	                                                    buffer.length);
+	        
+	        String a = "0:test:localhost:808";
+			request.setAddress(bhost);
+			request.setData(a.getBytes());
+			request.setPort(8089);
+			
+			bSocket.send(request);
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
