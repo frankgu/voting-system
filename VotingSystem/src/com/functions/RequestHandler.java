ed04126d5e7b4e3ee51997af0b19e7761a667b88
shawn24 <shawn_yao@hotmail.com>
2015-04-04 17:25:14 -0400

    handler

---
+++ VotingSystem/src/com/functions/RequestHandler.java
@@ -0,0 +1,123 @@
+package com.functions;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
+
+public class RequestHandler implements Runnable {
+
+	private DatagramPacket request;
+	private int port;
+	private InetAddress host;
+	private DatagramSocket aSocket;
+	private String reqData;
+
+	public RequestHandler(String str, InetAddress addr, int iport, int p, String h) {
+
+        reqData = "";
+        reqData = str;
+		port = p;
+		try {
+			host = InetAddress.getByName(h);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		byte[] buffer = new byte[80];
+        DatagramPacket request = new DatagramPacket(buffer,
+                                                    buffer.length);
+		request.setAddress(addr);
+		request.setData(str.getBytes());
+		request.setPort(iport);
+
+		/*try {
+			aSocket = new DatagramSocket(p, host);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+
+	public void run() {
+		String rtnMsg = new String(reqData);
+		String data = rtnMsg.split(":")[1]+":"+rtnMsg.split(":")[2]+":"+rtnMsg.split(":")[3];
+
+		try {
+			System.out.println("Processing request content: "
+					+ new String(reqData) + "\n");
+
+			if (rtnMsg.split(":")[0].equals("0")) {// add server
+				File inputFile = new File("sl.txt");
+				File tempFile = new File("slTempFile.txt");
+
+				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+				BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+				String lineToRemove = data;
+				String currentLine;
+
+				while((currentLine = reader.readLine()) != null) {
+				    // trim newline when comparing with lineToRemove
+				    String trimmedLine = currentLine.trim();
+				    if(trimmedLine.equals(lineToRemove)) continue;
+				    writer.write(currentLine + System.getProperty("line.separator"));
+				}
+				writer.close(); 
+				reader.close(); 
+				boolean successful = tempFile.renameTo(inputFile);
+				
+				Writer output;
+				output = new BufferedWriter(new FileWriter(inputFile, true));
+				output.append(System.getProperty("line.separator"));
+				output.append(data);
+				output.close();
+
+			} else if (rtnMsg.split(":")[0].equals("1")) {//del server
+				File inputFile = new File("sl.txt");
+				File tempFile = new File("slTemp.txt");
+
+				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+				BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+				String lineToRemove = data;
+				String currentLine;
+
+				while((currentLine = reader.readLine()) != null) {
+				    // trim newline when comparing with lineToRemove
+				    String trimmedLine = currentLine.trim();
+				    if(trimmedLine.equals(lineToRemove)) continue;
+				    writer.write(currentLine + System.getProperty("line.separator"));
+				}
+				writer.close(); 
+				reader.close(); 
+				boolean successful = tempFile.renameTo(inputFile);
+			} else {
+				//do nothing
+			}
+			/*DatagramPacket reply = new DatagramPacket(request
+                    .getData(), request.getLength(), request
+                    .getAddress(), request.getPort());
+
+			aSocket.send(reply);*/
+		} catch (SocketException e) {
+			System.out.println("Socket: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IO: " + e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+		}
+	}
+
+}
---
+++ VotingSystem/src/com/run/ServerListManager.java
@@ -0,0 +1,52 @@
+package com.run;
+
+
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.*;
+import java.util.Random;
+import java.util.Stack;
+
+import com.functions.*;
+
+public class ServerListManager {
+    
+    public static DatagramSocket aSocket;
+    
+    
+    
+    // public static int threadIndex = 0;
+    
+    public static void main(String args[]) throws Exception {
+        
+        ExecutorService executor = Executors.newFixedThreadPool(50);
+        // DatagramSocket aSocket = null;
+        final Semaphore sem = new Semaphore(1);
+        if (args.length < 2) {
+            System.out.println("Usage: java UDPServer <Host IP> <Port Number>");
+            System.exit(1);
+        }
+
+        try {
+            int socket_no = Integer.valueOf(args[1]).intValue();
+			InetAddress aHost = InetAddress.getByName(args[0]);
+			
+            aSocket = new DatagramSocket(socket_no, aHost);
+            
+            while (true) {
+
+        		
+                byte[] buffer = new byte[80];
+                DatagramPacket request = new DatagramPacket(buffer,
+                                                            buffer.length);
+                aSocket.receive(request);
+                System.out.println("new ...");
+                //Thread.sleep(2000);
+                new Thread(new RequestHandler(new String(request.getData()), request.getAddress(),request.getPort(), socket_no, args[0])).start();
+            }
+            
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        }
+    }
+}
