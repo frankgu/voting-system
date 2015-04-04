8185eb6df61af6383cb258f60f0eba1117feb364
shawn24 <shawn_yao@hotmail.com>
2015-04-04 18:35:57 -0400

    handler

--- VotingSystem/src/com/functions/RequestHandler.java
+++ VotingSystem/src/com/functions/RequestHandler.java
@@ -9,8 +9,10 @@ import java.net.UnknownHostException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.Writer;
 
 public class RequestHandler implements Runnable {
@@ -85,33 +87,7 @@ public class RequestHandler implements Runnable {
 				output.close();
 
 			} else if (rtnMsg.split(":")[0].equals("1")) {//del server
-				File inputFile = new File("sl.txt");
-				File tempFile = new File("slTemp.txt");
-
-				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-				BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-
-				String lineToRemove = "";
-				lineToRemove = data;
-
-				
-				String currentLine = "";
-
-				while((currentLine = reader.readLine()) != null) {
-				    // trim newline when comparing with lineToRemove
-				    String trimmedLine = currentLine.trim();
-
-			    	System.out.println("currLine: "+trimmedLine);
-			    	System.out.println("line: "+lineToRemove);
-			    	
-				    if(currentLine.equals(lineToRemove)){
-				    	continue;
-				    }
-				    writer.write(currentLine + System.getProperty("line.separator"));
-				}
-				writer.close(); 
-				reader.close(); 
-				boolean successful = tempFile.renameTo(inputFile);
+				removeLineFromFile("sl.txt", data);
 			} else {
 				//do nothing
 			}
@@ -130,5 +106,56 @@ public class RequestHandler implements Runnable {
 
 		}
 	}
+	
+	public void removeLineFromFile(String file, String lineToRemove) {
+
+	    try {
+
+	      File inFile = new File(file);
+
+	      if (!inFile.isFile()) {
+	        System.out.println("Parameter is not an existing file");
+	        return;
+	      }
+
+	      //Construct the new file that will later be renamed to the original filename.
+	      File tempFile = new File(inFile.getAbsolutePath() + ".tmp");
+
+	      BufferedReader br = new BufferedReader(new FileReader(file));
+	      PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+
+	      String line = null;
+
+	      //Read from the original file and write to the new
+	      //unless content matches data to be removed.
+	      while ((line = br.readLine()) != null) {
+
+	        if (!line.trim().equals(lineToRemove)) {
+
+	          pw.println(line);
+	          pw.flush();
+	        }
+	      }
+	      pw.close();
+	      br.close();
+
+	      //Delete the original file
+	      if (!inFile.delete()) {
+	        System.out.println("Could not delete file");
+	        return;
+	      }
+
+	      //Rename the new file to the filename the original file had.
+	      if (!tempFile.renameTo(inFile))
+	        System.out.println("Could not rename file");
+
+	    }
+	    catch (FileNotFoundException ex) {
+	      ex.printStackTrace();
+	    }
+	    catch (IOException ex) {
+	      ex.printStackTrace();
+	    }
+	  }
 
 }
