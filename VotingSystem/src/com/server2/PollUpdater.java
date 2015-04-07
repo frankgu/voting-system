aa03e223b03bdf378c8296e494006b19dacd58f5
Thomas Bigelow <thomasbigelow@cmail.carleton.ca>
2015-04-07 15:16:20 -0400

    display results based on district

--- VotingSystem/src/com/server2/PollUpdater.java
+++ VotingSystem/src/com/server2/PollUpdater.java
@@ -13,11 +13,13 @@ import com.object.Candidate;
 
 public class PollUpdater extends JFrame implements Runnable{
 	Server2GUIPanel panel;
-	//the list of votes and get method. Populated by getCandidatePolls
-	private List<Candidate> votes;
+	//the list of candidateList and get method. Populated by getCandidatePolls
+	private List<Candidate> candidateList;
 	private ArrayList <Candidate> winner;
+	private ArrayList <String> districts;
 	
 	public PollUpdater(){
+
 		// initial the hibernate factory
 		HibernateUtil.getSessionFactory();
 				
@@ -36,18 +38,28 @@ public class PollUpdater extends JFrame implements Runnable{
 	public void run(){
 		while(true){
 			try{
+				//retrieve the list of candidates from the database and update the bar graph based on which district is selected
 				updateCandidateList();
-				panel.setData(votes);
+				List<Candidate> temp = new ArrayList<Candidate>();
+				if (panel.getSelectedDistrict()!="All Districts"){
+					for(int i = 0; i<candidateList.size(); i++){
+						if (candidateList.get(i).getDistrictName().equals(panel.getSelectedDistrict())){
+							temp.add(candidateList.get(i));
+						}
+					}
+				}else temp = candidateList;
+				panel.setData(temp);
 				
 				updateWinner();
 				panel.setWinner(winner);
 				
+				updateDistricts();
+				panel.setDistricts(districts);
 				//update the size of the frame based on the number of candidates
-				setSize(votes.size()*100 + 125, 500);
+				setSize(temp.size()*100 + 125, 500);
 				
-
 				panel.repaint();
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			}
 			catch(Exception e){
 				
@@ -65,7 +77,7 @@ public class PollUpdater extends JFrame implements Runnable{
 		
 		List<Candidate> candidates = session.createCriteria(Candidate.class)
 				.list();
-		votes = candidates;
+		candidateList = candidates;
 		
 		session.getTransaction().commit();
 		session.close();
@@ -73,13 +85,26 @@ public class PollUpdater extends JFrame implements Runnable{
 	}
 	
 	private void updateWinner(){
-		List<Candidate> sortedVotes = new ArrayList<Candidate>(votes);
+		List<Candidate> sortedVotes = new ArrayList<Candidate>(candidateList);
 		Collections.sort(sortedVotes,new PollComparator());
 		winner = new ArrayList<Candidate>();
-		winner.add(sortedVotes.get(0));
-		for (int i=1; i<sortedVotes.size(); i++){
-			if (sortedVotes.get(i).getPolls()==winner.get(0).getPolls())
-				winner.add(sortedVotes.get(i));
+		if (!sortedVotes.isEmpty()){
+			winner.add(sortedVotes.get(0));
+			for (int i=1; i<sortedVotes.size(); i++){
+				if (sortedVotes.get(i).getPolls()==winner.get(0).getPolls())
+					winner.add(sortedVotes.get(i));
+			}
+		}
+	}
+	
+	private void updateDistricts(){
+		districts = new ArrayList<String>();
+		if (!candidateList.isEmpty()){
+			districts.add(candidateList.get(0).getDistrictName());
+			for(int i = 1; i<candidateList.size(); i++){
+				if (!districts.contains(candidateList.get(i).getDistrictName()))
+					districts.add(candidateList.get(i).getDistrictName());
+			}
 		}
 	}
 }
--- VotingSystem/src/com/server2/Server2GUIPanel.java
+++ VotingSystem/src/com/server2/Server2GUIPanel.java
@@ -3,23 +3,48 @@ package com.server2;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import com.object.Candidate;
 
 public class Server2GUIPanel extends JPanel {
-	
-	private List<Candidate> votes;
+	JFrame topFrame;
+	private List<Candidate> candidateList;
 	private ArrayList<Candidate> winner;
 	private ArrayList<Color> colors;
+	private ArrayList<String> districts;
+	JComboBox<String> districtComboBox;
+	private int districtNum;
+	private String selectedDistrict;
 	
 	public Server2GUIPanel(){
-		votes = new ArrayList<Candidate>();
+		
+		candidateList = new ArrayList<Candidate>();
 		colors = new ArrayList<Color>();
 		winner = new ArrayList<Candidate>();
+		districts = new ArrayList<String>();
+		districtNum = 0;
+		districtComboBox = new JComboBox<String>();
+        districtComboBox.addActionListener(
+        	new ActionListener(){
+        		public void actionPerformed(ActionEvent e){
+                	JComboBox<String> combo = (JComboBox<String>)e.getSource();
+                    selectedDistrict = (String)combo.getSelectedItem();
+                }
+        	} 
+        	
+        );
+		
+		add(districtComboBox);
+		
 		colors.add(Color.blue);
 		colors.add(Color.green);
 		colors.add(Color.yellow);
@@ -34,18 +59,35 @@ public class Server2GUIPanel extends JPanel {
 		colors.add(Color.red);
 	}
 	
-	public void setData(List<Candidate> votes){
-		this.votes = votes;
+	public void setData(List<Candidate> candidateList){
+		this.candidateList = candidateList;
 	}
 	
 	public void setWinner(ArrayList<Candidate> winner){
 		this.winner = winner;
 	}
 	
+	public void setDistricts(ArrayList<String> districts){
+		//only update the combo box if a new district has been created
+		if(districts.size() > districtNum){
+			districtNum = districts.size();
+			this.districts = districts;
+			districtComboBox.removeAllItems();
+			districtComboBox.addItem("All Districts");
+			for(int i = 0; i<districts.size(); i++){
+				districtComboBox.addItem(districts.get(i));
+			}
+		}
+	}
+	
+	public String getSelectedDistrict(){
+		return selectedDistrict;
+	}
+	
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		
+
 		//scale
 		g.setColor(Color.black);
 		for(int i = 0; i<5; i++){
@@ -53,7 +95,7 @@ public class Server2GUIPanel extends JPanel {
 			g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
 			g.drawString(""+i*50, 15, 350-i*50);
 			//the line
-			g.drawLine(70, 350-i*50, votes.size()*100+70, 350-i*50);
+			g.drawLine(70, 350-i*50, candidateList.size()*100+70, 350-i*50);
 		}
 		
 		//title
@@ -74,8 +116,8 @@ public class Server2GUIPanel extends JPanel {
 			
 		
 		//vote bar graph
-		for(int i = 0; i<votes.size(); i++){
-			int count = votes.get(i).getPolls();
+		for(int i = 0; i<candidateList.size(); i++){
+			int count = candidateList.get(i).getPolls();
 			//the bar
 			g.setColor(colors.get(i));
 			g.fillRect(i*100 + 70, 350, 75, -count);
@@ -84,8 +126,8 @@ public class Server2GUIPanel extends JPanel {
 			g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
 			g.drawString(""+count, i*100 + 100, 350 - count - 5);
 			//legend
-			g.drawString(votes.get(i).getFirstName(), i*100 + 70, 400);
-			g.drawString(votes.get(i).getLastName(), i*100 + 70, 425);
+			g.drawString(candidateList.get(i).getFirstName(), i*100 + 70, 400);
+			g.drawString(candidateList.get(i).getLastName(), i*100 + 70, 425);
 		}
 		
 		
