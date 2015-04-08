1b759e39c5c9dc51d938cca8cad4f747dd9345b6
Dongfeng Gu <gdf1992803@hotmail.com>
2015-04-07 21:39:28 -0400

    create the server2 exec file and change the server2 port to 9090

--- VotingSystem/src/com/run/StartServer2.java
+++ VotingSystem/src/com/run/StartServer2.java
@@ -4,7 +4,7 @@ import com.server2.Server2;
 public class StartServer2{
 
 	public static void main(String args[]){
-		Server2 server2 = new Server2(8080, "127.0.0.1");
+		Server2 server2 = new Server2(9090, "127.0.0.1");
 		Thread thread = new Thread(server2);
 		thread.start();
 	}
