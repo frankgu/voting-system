86c90f1d483b3956dbbabedf315157f627dc6c9e
Dongfeng Gu <gdf1992803@hotmail.com>
2015-04-07 23:00:34 -0400

    update

--- VotingSystem/src/com/run/StartServer2.java
+++ VotingSystem/src/com/run/StartServer2.java
@@ -6,9 +6,9 @@ public class StartServer2{
 	public static void main(String args[]){
 		Server2 server2;
 		if(!args[0].isEmpty()){
-			server2 = new Server2(Integer.parseInt(args[0]), "127.0.0.1");
+			server2 = new Server2(Integer.parseInt(args[0]), "go.joyclick.org");
 		} else {
-			server2 = new Server2(8080, "127.0.0.1");
+			server2 = new Server2(8080, "go.joyclick.org");
 		}
 		Thread thread = new Thread(server2);
 		thread.start();
